45a31452105f0e815497d6530118ee10e25fdf89
James Newell <james@digitaledgeit.com.au>
2014-12-02 01:21:26 +1100

    added .use() method separate to queue method

--- test/browserbot-plugin.js
+++ test/browserbot-plugin.js
@@ -1,19 +1,15 @@
-var fs = require('fs');
 var assert = require('assert');
-var server = require('./server');
 var BrowserBot = require('../browserbot');
 
-describe('BrowserBot::Plugin', function() {
+describe('BrowserBot plugins', function() {
 
-	describe('.queue()', function() {
+	describe('.use()', function() {
 
-		it('should queue', function(done) {
+		it('should be called with an instance of the object', function(done) {
 
 			BrowserBot()
-				.queue(function(browserbot, done) {
+				.use(function(browserbot) {
 					assert(browserbot instanceof BrowserBot);
-					assert(typeof(done) === 'function');
-					done();
 				})
 				.run(function(err) {
 					assert.equal(typeof(err), typeof(undefined));
